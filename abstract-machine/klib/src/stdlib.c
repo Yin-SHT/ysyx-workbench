dda4739eefa4fbc9865472362c242f57852ad53a
yinhaofei <yinhf2022@shanghaitech.edu.cn>
2023-08-10 08:21:52 +0800

    delete unused attribute

--- abstract-machine/klib/src/stdlib.c
+++ abstract-machine/klib/src/stdlib.c
@@ -4,7 +4,7 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 static unsigned long int next = 1;
-static char *hbrk __attribute__((unused)) = NULL;
+static char *hbrk = NULL;
 
 int rand(void) {
   // RAND_MAX assumed to be 32767
