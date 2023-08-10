12bbe98ffdaa84cc0e2cbac08ea100b45856b50f
yinhaofei <yinhf2022@shanghaitech.edu.cn>
2023-08-10 17:43:11 +0800

    add kerboard

--- abstract-machine/am/src/platform/nemu/ioe/input.c
+++ abstract-machine/am/src/platform/nemu/ioe/input.c
@@ -2,8 +2,10 @@
 #include <nemu.h>
 
 #define KEYDOWN_MASK 0x8000
+#define SCANCODE_MASK 0x000000FF
 
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
-  kbd->keydown = 0;
-  kbd->keycode = AM_KEY_NONE;
+  uint32_t kbd_reg = inl(KBD_ADDR);
+  kbd->keydown = (kbd_reg & KEYDOWN_MASK) >> 12;
+  kbd->keycode = kbd_reg & SCANCODE_MASK;
 }
