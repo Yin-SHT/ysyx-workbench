4b4fa58df3196015538f4ff79edbb2a32916d1de
yinhaofei <yinhf2022@shanghaitech.edu.cn>
2023-09-01 16:11:27 +0800

    pal 已实现按键
    但是不知道是不是缺少字幕文件的原因，
    画面不能正常显示台词

--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -10,56 +10,60 @@ static const char *keyname[] = {
   _KEYS(keyname)
 };
 
+#define NR_KEYS (sizeof(keyname) / sizeof(keyname[0]))
+static uint8_t keystate[NR_KEYS] = {0};
+
 int SDL_PushEvent(SDL_Event *ev) {
   return 0;
 }
 
-int SDL_PollEvent(SDL_Event *ev) {
-  char buf[64];
-  if (!NDL_PollEvent(buf, sizeof(buf))) {
-    return 0;
-  }
+static void process_event(char *buf, SDL_Event *event) {
+  SDL_Event ev;
   char kmotion[32];
   char key[32];
+
+  /* Process keyboard event */
   sscanf(buf, " %s %s ", kmotion, key);
   if (!strcmp(kmotion, "kd")) {
-    ev->key.type =  SDL_KEYDOWN;
+    ev.key.type =  SDL_KEYDOWN;
   } else if (!strcmp(kmotion, "ku")) {
-    ev->key.type =  SDL_KEYUP;
+    ev.key.type =  SDL_KEYUP;
   } else {
-    // TODO
+    assert(0);
   }
   uint8_t sym = 0;
-  for (; sym < 64; sym++) {
+  for (; sym < NR_KEYS; sym++) {
     if (!strcmp(keyname[sym], key)) {
       break;
     }
   }
-  ev->key.keysym.sym = sym;
-  return 1;
+  ev.key.keysym.sym = sym;
+
+  if (event) {
+    *event = ev;
+    keystate[event->key.keysym.sym] = (event->key.type == SDL_KEYDOWN) ? 1 : 0;
+  }
+}
+
+int SDL_PollEvent(SDL_Event *event) {
+  char buf[64];
+
+  if (NDL_PollEvent(buf, sizeof(buf))) {
+    process_event(buf, event);
+    return 1;
+  }
+
+  /* no event */
+  return 0;
 }
 
 int SDL_WaitEvent(SDL_Event *event) {
   char buf[64];
+
+  /* Waits indefinitely for the next available event */
   while (1) {
     if (NDL_PollEvent(buf, sizeof(buf))) {
-      char kmotion[32];
-      char key[32];
-      sscanf(buf, " %s %s ", kmotion, key);
-      if (!strcmp(kmotion, "kd")) {
-        event->key.type =  SDL_KEYDOWN;
-      } else if (!strcmp(kmotion, "ku")) {
-        event->key.type =  SDL_KEYUP;
-      } else {
-        // TODO
-      }
-      uint8_t sym = 0;
-      for (; sym < 64; sym++) {
-        if (!strcmp(keyname[sym], key)) {
-          break;
-        }
-      }
-      event->key.keysym.sym = sym;
+      process_event(buf, event);
       return 0;
     }
   }
@@ -72,5 +76,5 @@ int SDL_PeepEvents(SDL_Event *ev, int numevents, int action, uint32_t mask) {
 }
 
 uint8_t* SDL_GetKeyState(int *numkeys) {
-  return NULL;
+  return keystate;
 }
