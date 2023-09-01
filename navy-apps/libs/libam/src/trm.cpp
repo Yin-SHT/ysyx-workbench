e6ef9c4a1e1a51fe72ad22160a5a6fb0abe56a1c
yinhaofei <yinhf2022@shanghaitech.edu.cn>
2023-09-01 18:35:19 +0800

    error

--- navy-apps/libs/libam/src/trm.cpp
+++ navy-apps/libs/libam/src/trm.cpp
@@ -2,8 +2,19 @@
 
 Area heap;
 
+#define DEVICE_BASE 0xa0000000
+#define SERIAL_PORT (DEVICE_BASE + 0x00003f8)
+#define nemu_trap(code) asm volatile("mv a0, %0; ebreak" : :"r"(code))
+
+static inline void outb(uintptr_t addr, uint8_t  data) { *(volatile uint8_t  *)addr = data; }
+
 void putch(char ch) {
+  outb(SERIAL_PORT, ch);
 }
 
 void halt(int code) {
+  nemu_trap(code);
+
+  // should not reach here
+  while (1);
 }
