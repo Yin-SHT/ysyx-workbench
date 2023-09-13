20c70612810da9824d02f6c534758176397ffca6
yinhaofei <yinhf2022@shanghaitech.edu.cn>
2023-09-13 15:55:54 +0800

    键盘 events 通过

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -27,7 +27,7 @@ size_t serial_write(const void *buf, size_t offset, size_t len) {
 size_t events_read(void *buf, size_t offset, size_t len) {
   AM_INPUT_KEYBRD_T ev = io_read(AM_INPUT_KEYBRD);
   if (ev.keycode != AM_KEY_NONE) {
-    return snprintf(buf, len, " %s %s ", ev.keydown ? "kd" : "ku", keyname[ev.keycode]);
+    return snprintf(buf, len, "%s %s\n", ev.keydown ? "kd" : "ku", keyname[ev.keycode]);
   }
   return 0;
 }
