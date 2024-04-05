4bd7c35424301c56a9a088e60d7cf99f78a65162
yinhaofei <yinhf2022@shanghaitech.edu.cn>
2024-04-05 15:33:35 +0800

    bootloader 将程序加载到 psram 中执行

--- abstract-machine/am/src/riscv/ysyxSoC/trm.c
+++ abstract-machine/am/src/riscv/ysyxSoC/trm.c
@@ -8,36 +8,22 @@ int main(const char *args);
 
 extern char _heap_start;
 
-Area heap = {.start = (void *)&_heap_start, .end = (void *)0x0f002000}; 
+Area heap = {.start = (void *)&_heap_start, .end = (void *)0x80400000}; 
 #ifndef MAINARGS
 #define MAINARGS ""
 #endif
 static const char mainargs[] = MAINARGS;
 
 // Temp Device Addr, these will be changed in the future !!!
-//#define DEVICE_BASE 0xa0000000
 #define SERIAL_PORT 0x10000000  // uart16550
 
-extern char _rodata_end;
-extern char _data_start, _data_end;
-extern char _bss_start, _bss_end;
-
-static bool uart_init = false;
-
 void putch(char ch) {
-  if (!uart_init) {
-    uint8_t lcr = inb(SERIAL_PORT + 3);
-    outb(SERIAL_PORT + 3, lcr | 0x80);
-    outb(SERIAL_PORT + 1, 0x00);
-    outb(SERIAL_PORT + 0, 0x01);
-    outb(SERIAL_PORT + 3, lcr & 0x7f);
-    uart_init = true;
-  }
-
+  // Polling
   uint8_t lsr = inb(SERIAL_PORT + 5);
   while (lsr & 0x02) {
     lsr = inb(SERIAL_PORT + 5);
   }
+
   outb(SERIAL_PORT, ch);
 }
 
@@ -48,17 +34,24 @@ void halt(int code) {
 }
 
 void _trm_init() {
-  // ID
-//  uint32_t mvendorid = 0;
-//  uint32_t marchid = 0;
-//  __asm__ __volatile__(
-//		"csrr %0, mvendorid;"
-//    "csrr %1, marchid;" 
-//		: "=r"(mvendorid), "=r"(marchid) ::               
-//  );
-//
-//  printf("mvendorid: 0x%08x\n", mvendorid);
-//  printf("marchid: %d\n", marchid);
+  // Initialize uart16550
+  uint8_t lcr = inb(SERIAL_PORT + 3);
+  outb(SERIAL_PORT + 3, lcr | 0x80);
+  outb(SERIAL_PORT + 1, 0x00);
+  outb(SERIAL_PORT + 0, 0x01);
+  outb(SERIAL_PORT + 3, lcr & 0x7f);
+
+  // Identification
+  uint32_t mvendorid = 0;
+  uint32_t marchid = 0;
+  __asm__ __volatile__(
+		"csrr %0, mvendorid;"
+    "csrr %1, marchid;" 
+		: "=r"(mvendorid), "=r"(marchid) ::               
+  );
+
+  printf("mvendorid: 0x%08x\n", mvendorid);
+  printf("marchid:   %d\n", marchid);
 
   int ret = main(mainargs);
   halt(ret);
