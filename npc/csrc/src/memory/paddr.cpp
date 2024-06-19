1e785bfe43efafae0d304c0184de6b78115c8224
yinhaofei <yinhf2022@shanghaitech.edu.cn>
2024-06-19 15:10:53 +0800

    改进 axi_read/write 缩进

--- npc/csrc/src/memory/paddr.cpp
+++ npc/csrc/src/memory/paddr.cpp
@@ -14,12 +14,12 @@ uint8_t* guest_to_host(paddr_t paddr) { return pmem + paddr - CONFIG_MBASE; }
 paddr_t host_to_guest(uint8_t *haddr) { return haddr - pmem + CONFIG_MBASE; }
 
 static void pmem_read(uint32_t araddr, uint32_t *rdata_high, uint32_t *rdata_low) {
-  uint64_t *PMEM = (uint64_t *)pmem;
-  uint64_t offset = (araddr - 0x80000000) >> 3;
-  uint64_t content = PMEM[offset];
+    uint64_t *PMEM = (uint64_t *)pmem;
+    uint64_t offset = (araddr - 0x80000000) >> 3;
+    uint64_t content = PMEM[offset];
 
-  *rdata_high = (uint32_t)(content >> 32);
-  *rdata_low  = (uint32_t)(content);
+    *rdata_high = (uint32_t)(content >> 32);
+    *rdata_low  = (uint32_t)(content);
 }
 
 static void pmem_write(uint32_t awaddr, uint32_t wdata_high, uint32_t wdata_low, uint32_t wstrb) {
@@ -39,56 +39,56 @@ static void pmem_write(uint32_t awaddr, uint32_t wdata_high, uint32_t wdata_low,
 }
 
 extern "C" void axi4_read(uint32_t araddr, uint32_t *rdata_high, uint32_t *rdata_low, uint32_t arsize) {
-  if (likely(in_pmem(araddr))) {
-    pmem_read(araddr, rdata_high, rdata_low);
-    return;
-  }
+    if (likely(in_pmem(araddr))) {
+        pmem_read(araddr, rdata_high, rdata_low);
+        return;
+    }
 
-  int len = (arsize == 0) ? 1 :
-            (arsize == 1) ? 2 :
-            (arsize == 2) ? 4 : 
-            (arsize == 3) ? 8 : 1024;
-  word_t data = mmio_read(araddr, len);
-  if ((araddr % 8) == 0) {
-    *rdata_high = 0;
-    *rdata_low = data;
-  } else if ((araddr % 8) == 4) {
-    *rdata_high = data;
-    *rdata_low = 0;
-  } else {
-    panic("Unsupported offset %d\n", araddr % 8);
-  }
+    int len =   (arsize == 0) ? 1 :
+                (arsize == 1) ? 2 :
+                (arsize == 2) ? 4 : 
+                (arsize == 3) ? 8 : 1024;
+    word_t data = mmio_read(araddr, len);
+    if ((araddr % 8) == 0) {
+        *rdata_high = 0;
+        *rdata_low = data;
+    } else if ((araddr % 8) == 4) {
+        *rdata_high = data;
+        *rdata_low = 0;
+    } else {
+        panic("axi4_read: unsupported offset %d\n", araddr % 8);
+    }
 }
 
 extern "C" void axi4_write(uint32_t awaddr, uint32_t wdata_high, uint32_t wdata_low, uint32_t awsize, uint32_t wstrb) {
-  if (likely(in_pmem(awaddr))) {
-    pmem_write(awaddr, wdata_high, wdata_low, wstrb);
-    return;
-  }
-  
-  uint64_t wdata = (((uint64_t)wdata_high) << 32) | ((uint64_t)wdata_low);
-  int len = (awsize == 0) ? 1 :
-            (awsize == 1) ? 2 :
-            (awsize == 2) ? 4 : 
-            (awsize == 3) ? 8 : 1024;
-  int cnt = 0, bit = wstrb & 0x1;
-  while (!bit) {
-    cnt ++;
-    wstrb >>= 1;
-    bit = wstrb & 0x1;
-  }
-  for (int i = 0; i < cnt; i ++) {
-    wdata >>= 8;
-  }
-  word_t data = (word_t)wdata;
-  mmio_write(awaddr, len, data);
+    if (likely(in_pmem(awaddr))) {
+        pmem_write(awaddr, wdata_high, wdata_low, wstrb);
+        return;
+    }
+    
+    uint64_t wdata = (((uint64_t)wdata_high) << 32) | ((uint64_t)wdata_low);
+    int len =   (awsize == 0) ? 1 :
+                (awsize == 1) ? 2 :
+                (awsize == 2) ? 4 : 
+                (awsize == 3) ? 8 : 1024;
+    int cnt = 0, bit = wstrb & 0x1;
+    while (!bit) {
+        cnt ++;
+        wstrb >>= 1;
+        bit = wstrb & 0x1;
+    }
+    for (int i = 0; i < cnt; i ++) {
+        wdata >>= 8;
+    }
+    word_t data = (word_t)wdata;
+    mmio_write(awaddr, len, data);
 }
 
 void init_mem() {
-  uint32_t *p = (uint32_t *)pmem;
-  int i;
-  for (i = 0; i < (int) (CONFIG_MSIZE / sizeof(p[0])); i ++) {
-    p[i] = rand();
-  }
-  Log("physical memory area [" FMT_PADDR ", " FMT_PADDR "]", PMEM_LEFT, PMEM_RIGHT);
+    uint32_t *p = (uint32_t *)pmem;
+    int i;
+    for (i = 0; i < (int) (CONFIG_MSIZE / sizeof(p[0])); i ++) {
+        p[i] = rand();
+    }
+    Log("physical memory area [" FMT_PADDR ", " FMT_PADDR "]", PMEM_LEFT, PMEM_RIGHT);
 }
