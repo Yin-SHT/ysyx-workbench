ff8a2c28d68976f463269ca561fa3334c060847e
Yin-SHT <yinhf2022@shanghaitech.edu.cn>
2024-07-02 16:18:09 +0800

    完成 pa3 阶段2 “系统调用”
    通过 native 和 riscv32-nemu 架构测试

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -1,8 +1,10 @@
 #include <common.h>
 
+void do_syscall(Context *c);
+
 static Context* do_event(Event e, Context* c) {
   switch (e.event) {
-    case EVENT_SYSCALL: printf("Syscall event!\n"); break;
+    case EVENT_SYSCALL: do_syscall(c); break;
     case EVENT_YIELD: printf("Yield event!\n"); break;
     default: panic("Unhandled event ID = %d", e.event);
   }
